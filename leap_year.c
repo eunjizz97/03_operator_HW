417e6e8d7a030e4e2f9abd9d1f760fa78af5c15e
eunjizz97 <42995763+eunjizz97@users.noreply.github.com>
2018-10-02 21:44:10 +0900

    leap year
    김주엽 교수님 여기에 질문해도 되는거 맞나요..
    %d 하고 %i 의 차이는 무엇인가요?

---
+++ leap_year.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int year;
+	int result;
+	
+	printf("input the year : ");
+	scanf("%d", &year);
+	
+	result = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+	
+	printf("is the year %d the leap year? : %d", year, result);
+	
+	return 0;
+}
